9f53ba9519185714d49dd4be8e74b407eb3c93cb
robin <robin.schmidt.97@web.de>
2016-11-12 18:32:11 +0100

    Add Party Fragment layout added (with scrollable view); preference layout  added

--- app/src/main/java/com/julianriegraf/app2night/SettingsFragment.java
+++ app/src/main/java/com/julianriegraf/app2night/SettingsFragment.java
@@ -1,21 +1,24 @@
 package com.julianriegraf.app2night;
 
+
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.preference.PreferenceFragmentCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 
 /**
  * Created by robin on 15.10.2016.
  */
 
-public class SettingsFragment extends Fragment {
-    @Nullable
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.settings, container, false);
-        return v;
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.preferences, rootKey);
     }
 }
